4ce4693c3f8c06c5c2a3c02173ef979cdc03bde4
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-12 10:09:58 +0000

    Setup default url options for link generation in active_model_serializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options = {
+    host: 'pasco-api-staging.herokuapp.com'
+  }
 end
