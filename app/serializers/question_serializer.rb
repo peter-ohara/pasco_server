cf681ebbf76e5701c4e4d78b113cfe8517de09e8
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-13 12:50:19 +0000

    Add relationship links to api quiz and question endpoints

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,3 +1,12 @@
 class QuestionSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :priority, :number, :question, :choices, :answer, :explanation
+
+    attribute :links do
+      {
+        self: question_path(object),
+        quiz: quiz_path(object.quiz)
+      }
+    end
 end
--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,5 +1,14 @@
 class QuizSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :instructions, :duration, :quiz_type, :year
 
   has_many(:questions)
+
+  attribute :links do
+    {
+      self: quiz_path(object),
+      questions: quiz_questions_path(object)
+    }
+  end
 end
