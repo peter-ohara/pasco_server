9c3c55567ee3000c4ac41d8817d21241065b593e
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-13 10:39:29 +0000

    Write seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quiz = Quiz.create!(instructions: 'Here are some quiz instructions',
+                    duration: nil,
+                    quiz_type: 'mid_sem',
+                    year: '2010',
+                    course_code: 'CSM 157')
+
+
+quiz = Quiz.create!(instructions: 'Here are some quiz instructions',
+                    duration: nil,
+                    quiz_type: 'end_of_sem',
+                    year: '2010',
+                    course_code: 'CSM 157')
+
+
+quiz = Quiz.create!(instructions: 'Here are some quiz instructions',
+                    duration: nil,
+                    quiz_type: 'class_test',
+                    year: '2010',
+                    course_code: 'CSM 157')
+
+
+quiz = Quiz.create!(instructions: 'Here are some quiz instructions',
+                    duration: nil,
+                    quiz_type: 'assignment',
+                    year: '2010',
+                    course_code: 'CSM 157')
+
