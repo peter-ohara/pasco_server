d08c8932b6ae917d6610604d3a619dbe15d6bde4
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-13 10:14:33 +0000

    Move quiz_type to quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,3 @@
 class Question < ApplicationRecord
-  enum quiz_type: {
-    mid_sem: 0,
-    end_of_sem: 1,
-    class_test: 2,
-    assignment: 3
-  }
-
   belongs_to :quiz
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,10 @@
 class Quiz < ApplicationRecord
+  enum quiz_type: {
+      mid_sem: 0,
+      end_of_sem: 1,
+      class_test: 2,
+      assignment: 3
+  }
+
   has_many :questions, dependent: :destroy
 end
