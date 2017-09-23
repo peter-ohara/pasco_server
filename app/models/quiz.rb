a8af2377f18d840a17115e7c6201c9eeccdfe2e7
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-09-23 21:19:40 +0000

    Validations for quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,31 +3,44 @@ class Quiz < ApplicationRecord
   pg_search_scope :search_by_name,
                   against: %i[course_code course_name quiz_type year],
                   using: {
-                    tsearch: {
-                      prefix: true,
-                      any_word: true,
-                      highlight: {
-                        StartSel: '<start>',
-                        StopSel: '<stop>',
-                        MaxWords: 123,
-                        MinWords: 456,
-                        ShortWord: 4,
-                        HighlightAll: true,
-                        MaxFragments: 3,
-                        FragmentDelimiter: '&hellip;'
+                      tsearch: {
+                          prefix: true,
+                          any_word: true,
+                          highlight: {
+                              StartSel: '<start>',
+                              StopSel: '<stop>',
+                              MaxWords: 123,
+                              MinWords: 456,
+                              ShortWord: 4,
+                              HighlightAll: true,
+                              MaxFragments: 3,
+                              FragmentDelimiter: '&hellip;'
+                          }
                       }
-                    }
                   }
 
   enum quiz_type: {
-    mid_sem: 0,
-    end_of_sem: 1,
-    class_test: 2,
-    assignment: 3
+      mid_sem: 0,
+      end_of_sem: 1,
+      class_test: 2,
+      assignment: 3
   }
 
-  has_many :questions, -> { order 'priority ASC' },
+  has_many :questions, -> {order 'priority ASC'},
            dependent: :destroy, inverse_of: :quiz
+
+
+  validates :course_code, :course_name, :quiz_type, :year, presence: true
+
+  validates :year, numericality: {
+    only_integer: true,
+    greater_than: 2000,
+    less_than_or_equal_to: Date.current.year
+  }
+
+  validates :duration,
+            numericality: { greater_than: 0, less_than_or_equal_to: 24 },
+            allow_nil: true
 
   def name
     if quiz_type
