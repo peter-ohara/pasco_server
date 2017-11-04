c207fed3b5c14c390a1e246b7ae6a07523c3732d
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-11-04 14:42:56 +0000

    Validate user has enough pasco_gold before making a purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,13 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  validate :user_has_enough_pasco_gold
+
+  def user_has_enough_pasco_gold
+    if user.pasco_gold < course.price
+      errors.add(:user,
+                 'does not have enough pasco gold to purchase this course')
+    end
+  end
 end
