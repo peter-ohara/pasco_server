95255ecfa247aab858231551abc346ff99f2f778
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-11-04 12:43:10 +0000

    Add price and quiz counts to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,24 @@ class Course < ApplicationRecord
   def title
     "#{code} #{name}"
   end
+
+  def total_quiz_count
+    quizzes.count
+  end
+
+  def mid_sem_count
+    quizzes.mid_sem.count
+  end
+
+  def end_of_sem_count
+    quizzes.end_of_sem.count
+  end
+
+  def class_test_count
+    quizzes.class_test.count
+  end
+
+  def assignment_count
+    quizzes.assignment.count
+  end
 end
--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,10 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :code, :name, :level, :semester
+  attributes :id, :code, :name, :level, :semester, :price,
+             :total_quiz_count,
+             :mid_sem_count,
+             :end_of_sem_count,
+             :class_test_count,
+             :assignment_count
 
   has_many(:quizzes)
 end
