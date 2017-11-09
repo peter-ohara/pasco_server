7d8b67c17e266b3e68bf3696169913d47cc39762
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-11-09 15:42:17 +0000

    Subtract course price from users pasco gold when they make a purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,11 +3,19 @@ class Purchase < ApplicationRecord
   belongs_to :course
 
   validate :user_has_enough_pasco_gold
+  after_create :update_users_pasco_gold
 
   def user_has_enough_pasco_gold
     if user.pasco_gold < course.price
       errors.add(:user,
                  'does not have enough pasco gold to purchase this course')
+    end
+  end
+
+  def update_users_pasco_gold
+    user.pasco_gold = user.pasco_gold - course.price
+    unless user.save
+      raise ActiveRecord::Rollback
     end
   end
 end
